cfac6bea8dce76251cfb72b62ac0ece73b639749
Diego Rodriguez <diego.gabrielrodriguez@hotmail.com>
2021-07-05 16:54:49 -0300

    adding item list container

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import NavBar from './components/NavBar'
+import ItemListContainer from './components/ItemListContainer';
 
 function App() {
+
+  const greeting = 'Welcome to Bansky Store'
 
   return (
     <>
       <NavBar/>
+      <ItemListContainer greeting={greeting}/>
     </>
   );
 }
