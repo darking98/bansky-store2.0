c1e06d8aabfc0d37ae75eb0da1b6b1ab5e8d2e91
Diego Rodriguez <diego.gabrielrodriguez@hotmail.com>
2021-07-15 23:33:58 -0300

    adding async

--- src/components/ItemListContainer.js
+++ src/components/ItemListContainer.js
@@ -1,12 +1,26 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
 import ItemProduct from './ItemProduct'
-import { products } from '../products/products'
+
 
 const ItemListContainer = () => {
+
+    const [products, setProducts] = useState([]);
+
+    const getCategory = async () =>{
+        let llamada = fetch(`http://localhost:4000/products`);
+        llamada = await llamada;
+        llamada = await llamada.text();
+        setProducts(JSON.parse(llamada));
+    }
+
+    useEffect(()=>{
+        getCategory();
+    },[])
 
     return (
         <div className="item-list-container">
             {
+                products.length === 0 ? <h2>Loading...</h2> :
                 products.map(product => (
                     <ItemProduct product = {product}/>
                 ))
